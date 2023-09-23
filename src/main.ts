31426534774a88dc51ba7735971e938ec3e82d9f
semoleg1986 <semoleg1986@gmail.com>
2023-09-23 22:28:50 +0300

    feat: add swagger & connect db (Sat, Sep 23, 2023 10:28:50 PM)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Resume Service')
+    .setDescription('Resume service')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+  app.setGlobalPrefix('api');
   await app.listen(port);
 }
 bootstrap();
